4e2ed0296bb3509cb9d65baedb8f681426537b91
kashish <kashishsalvi06@gmail.com>
2026-04-24 12:48:22 +0530

    default route set to create-post

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import CreatePost from './pages/CreatePost'
 import Feed from './pages/Feed'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+
+        <Route path="/" element={<Navigate to="/create-post" />} />
+
         <Route path="/feed" element={<Feed />} />
         <Route path="/create-post" element={<CreatePost />} />
+
       </Routes>
     </BrowserRouter>
   )
